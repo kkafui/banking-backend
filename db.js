c8d6e0ac50e17194c6fa41c0634f1a8ce0e0626b
kkafui <kingsleykafui7@gmail.com>
2026-09-12 11:53:24 +0000

    Enhance database connection with SSL support
    Add SSL configuration for database connection based on host.

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const { Pool } = require("pg");
 
+const isLocalHost = ["localhost", "127.0.0.1"].includes(process.env.DB_HOST);
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: isLocalHost ? false : { rejectUnauthorized: false },
 });
 
 module.exports = pool;
