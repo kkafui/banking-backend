cad29ff1875f39c340a5b390d2331ca0f2c893b5
kkafui <kingsleykafui7@gmail.com>
2026-09-12 11:54:17 +0000

    Log health check errors to console
    Added console error logging for health check failures.

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.get("/api/health", async (req, res) => {
       databaseTime: result.rows[0].now,
     });
   } catch (error) {
+    console.error("HEALTH CHECK ERROR:", error); // TEMP — remove after debugging
+
     res.status(500).json({
       message: "Database connection failed",
     });
